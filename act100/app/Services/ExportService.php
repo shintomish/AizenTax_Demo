2690a8aab5335bee0756759520f5dd101abd41ad
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2023-10-10 14:37:01 +0900

    2023/10/10 sort input progress

--- act100/app/Services/ExportService.php
+++ act100/app/Services/ExportService.php
@@ -2,17 +2,34 @@
 
 namespace App\Services;
 
-use File;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx as XlsxReader;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx as XlsxWriter;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Maatwebsite\Excel\Facades\Excel;
+
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ExportService
 {
-    public function makePdf($to_name, $file_name, $foloder_name)
+    protected $proxies = "*";
+
+    // $export_service->makeXlsPdf($data['to_company'], $data['to_represent'] , $data['foloder_name'], $data['file_name']);
+    public function makeXlsPdf(
+                        $tourokuno,
+                        $tekiyou,
+                        $furibi,
+                        $from_company,
+                        $from_repres,
+                        $kanrino,
+                        $tanka,
+                        $to_company, 
+                        $to_represent, 
+                        $foloder_name, 
+                        $file_name
+                        )
     {
-        Log::info('ExportService makePdf START');
+        Log::info('ExportService makeXlsPdf START');
 
         // もとになるExcelを読み込み
         $excel_file = storage_path('app/public/invoice/xls/tmp/tmp_invoice.xlsx');
@@ -24,46 +41,64 @@ class ExportService
 
         // セルに指定した値挿入 M1 請求日
         $worksheet->setCellValue('M1', now()->format('Y/m/d'));
+        // セルに指定した値挿入 M2 登録番号
+        $worksheet->setCellValue('M2', $tourokuno);
+        // セルに指定した値挿入 K14 請求書宛名会社名
+        $worksheet->setCellValue('K14', $from_company);
+        // セルに指定した値挿入 K15 請求書宛名代表者名
+        $worksheet->setCellValue('K15', $from_repres);
+        // セルに指定した値挿入 K19 管理番号
+        $worksheet->setCellValue('K19', $kanrino);
+        // セルに指定した値挿入 K22 単価
+        $worksheet->setCellValue('K22', $tanka);
         // セルに指定した値挿入 B22 適用欄
-        $worksheet->setCellValue('B22', '2023年10月分　顧問料金xx');
-
+        $worksheet->setCellValue('B22', $tekiyou);
         // セルに指定した値挿入 C5 会社名 
-        $worksheet->setCellValue('C5', $to_name);
+        $worksheet->setCellValue('C5', $to_company);
+        // セルに指定した値挿入 C6 代表者名
+        $worksheet->setCellValue('C6', $to_represent. ' 様');
+        // セルに指定した値挿入 E55 ※大変恐縮ではございますが、　2023年4月15日　までにお振込みください
+        $worksheet->setCellValue('E55', $furibi);
 
-        // セルに指定した値挿入 C6 会社名 
-        $worksheet->setCellValue('C6', $to_name. ' 様');
+        // Dirなければ作成
+        if(!file_exists( storage_path('app/public/invoice/xls/'. $foloder_name))){
+            mkdir( storage_path('app/public/invoice/xls/'. $foloder_name), $mode = 0777, true);
+        }
+        if(!file_exists( storage_path('app/public/invoice/pdf/'. $foloder_name))){
+            mkdir( storage_path('app/public/invoice/pdf/'. $foloder_name), $mode = 0777, true);
+        }
 
         // Excel出力
+        $xls_path = 'app/public/invoice/xls/'. $foloder_name. '/'. $file_name. '.xlsx';
+        $export_xls_path = storage_path($xls_path);         //'app/invoice'
         $writer     = new XlsxWriter($spreadsheet);
-        $excel_path = '/app/public/invoice/xls/'. $foloder_name. '/'. $file_name. '.xlsx';
+        $writer->save($export_xls_path);
 
-        // $export_excel_path = storage_path($excel_path);     'app/public'
-        $export_excel_path = storage_path() .$excel_path;   // '/app/public'
-        $writer->save($export_excel_path);
- 
         // Pdf出力
-        if (file_exists($export_excel_path)) {
-            Log::info('ExportService makePdf Pdf出力 START');
+        if (file_exists($export_xls_path)) {
 
-            Log::debug('ExportService makePdf $export_excel_path = ' . $export_excel_path);
+            // Log::debug('ExportService makeXlsPdf $export_xls_path = ' . $export_xls_path);
 
-            // Excel -> Pdf
-            $this->convertOfficeToPdf($file_name, $foloder_name, $export_excel_path);
+            // ExcelファイルをPDFに変換するコード
+            $this->convertOfficeToPdf($file_name, $foloder_name, $export_xls_path);
 
-            Log::info('ExportService makePdf Pdf出力 END');
         }
-        Log::info('ExportService makePdf END');
+
+        Log::info('ExportService makeXlsPdf END');
         return;
     }
 
+    // Local commandではうまく行くが実行すると出来ない SV=OK
     public function convertOfficeToPdf($file_name,$foloder_name, $office_path)
     {
         Log::info('ExportService convertOfficeToPdf START');
 
         // putenv('HOME=/tmp'); // libreoffice の作業スペースとして tmp を使う
 
-        // $pdf_dir = storage_path('app/public/invoice/pdf/'. $foloder_name);
-        $pdf_dir = storage_path() . '/app/public/invoice/pdf/'. $foloder_name;
+        $pdf_dir = storage_path('app/public/invoice/pdf/'.  $foloder_name);
+        Log::debug('ExportService convertOfficeToPdf exec $pdf_dir = '    .print_r($pdf_dir,true));
+        // Log::debug('ExportService convertOfficeToPdf exec $foloder_name = '    .print_r($foloder_name,true));
+        // $office_path = './storage/app/public/invoice/xls/'. $foloder_name. '/'. $file_name. '.xlsx';
 
         #  /usr/bin/soffice --headless --convert-to pdf --outdir {出力先のディレクトリ} {変換する元のExcel}
         #  例) /tmp/sample.xls -> /tmp/sample.pdfに変換する場合
@@ -80,29 +115,22 @@ class ExportService
         //     $office_path
         // ];
         $command_parts = [
-            'export HOME=/tmp;',
+            'HOME=/tmp;',
             '/usr/bin/soffice',
-            '--language=ja',
             '--headless',
-            '--convert-to pdf:writer_pdf_Export',
+            '--language=ja',
+            '--convert-to pdf',
             '--outdir '. $pdf_dir,
             $office_path
         ];
         $command = implode(' ', $command_parts);
-        exec($command);
+        exec($command, $output, $return_var);
 
-        // OK Exec
-        // /usr/bin/soffice --language=ja --headless --convert-to pdf:writer_pdf_Export 
-        // --outdir ./storage/app/public/invoice/pdf/folder0002 
-        // ./storage/app/public/invoice/xls/folder0002/20231007_global_company-0002_請求書.xlsx
-
-        Log::debug('ExportService convertOfficeToPdf $command = ' . $command);
+        Log::debug('ExportService convertOfficeToPdf exec $office_path = '    .print_r($office_path,true));
+        Log::debug('ExportService convertOfficeToPdf exec $return_var = ' .print_r($return_var,true));
 
         // $filename = pathinfo($office_path, PATHINFO_FILENAME);
         $pdf_path = $pdf_dir . '/' . $file_name . '.pdf';
-        // $pdf_path = /var/www/html/storage/app/public/invoice/pdf/folder0002/20231007_global_company-0002_請求書.pdf  
-
-        Log::debug('ExportService convertOfficeToPdf $pdf_path = ' . $pdf_path);
 
         Log::info('ExportService convertOfficeToPdf END');
 
